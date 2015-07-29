b5ca828e949a751e82786bbdbffc4852d0fd4ff6
luolong <luolong0226@163.com>
2015-07-29 15:31:00 +0800

    IO读取部分

---
+++ io/file_read_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# File Read & Write
+# 2015-07-25 00:43:49
+
+# File Not Exist
+#f = open('/Users/luolong/test.txt', 'r')
+#print f
+
+# File Exist
+f = open('/Users/luolong/Code/LearnPython/jingyan.txt', 'r')
+
+print f.read()
+
+
