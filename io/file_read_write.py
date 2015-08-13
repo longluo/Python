4fba501b6a9b2d25f505ff19b9e562b26c3e4918
luolong <calmbody@163.com>
2015-08-13 10:48:41 +0800

    python对文件进行简单读写的方法

--- io/file_read_write.py
+++ io/file_read_write.py
@@ -13,4 +13,54 @@ f = open('/Users/luolong/Code/LearnPython/jingyan.txt', 'r')
 
 print f.read()
 
+f.close()
+
+# Make f always close
+try:
+	f = open('/Users/luolong/Code/LearnPython/jingyan.txt', 'r')
+	print f.read()
+finally:
+	if f:
+		f.close()
+
+# use with
+with open('/Users/luolong/Code/LearnPython/jingyan.txt', 'r') as f:
+	print f.read()
+
+# huge size file
+f = open('/Users/luolong/Code/LearnPython/jingyan.txt', 'r')
+print f.read(10)
+
+for line in f.readlines():
+	print(line.strip())
+
+
+# file-like object
+# open a image file.
+f = open('/Users/luolong/Code/LearnPython/xiaojing2.jpg', 'rb')
+print f.read()
+
+
+# write a file.
+f = open('/Users/luolong/Code/LearnPython/test.txt', 'w')
+f.write('Hello Python')
+f.close()
+
+# use with 
+with open('/Users/luolong/Code/LearnPython/test.txt', 'w') as f:
+	f.write('Hello, Python')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
